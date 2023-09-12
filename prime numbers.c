f4efcb94a0826ddcc6a7af1168ac818bec648a0b
Artem-Nesterenko2005 <144232708+Artem-Nesterenko2005@users.noreply.github.com>
2023-09-12 22:09:50 +0500

    Add files via upload

---
+++ prime numbers.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() 
+{
+	int array[5];
+	int symbolNumber = 0;
+	int counter = 0;
+	srand(time(0));
+	for (symbolNumber; symbolNumber < 5; symbolNumber++)
+	{
+		array[symbolNumber] = rand() % 3;
+		printf("%d, ", array[symbolNumber]);
+	}
+	symbolNumber = 0;
+	for (symbolNumber; symbolNumber < 5; symbolNumber++)
+	{
+		if (array[symbolNumber] == 0)
+			counter = counter + 1;
+	}
+	printf("\n%d", counter);
+}
